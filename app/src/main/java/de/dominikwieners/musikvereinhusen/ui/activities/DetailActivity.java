f95248d3b0310d84ac401d719bd4ff064ae92dd6
domw94 <dom.wieners@gmx.net>
2018-01-19 00:46:41 +0100

    fix CollapsingToolbar bug

--- app/src/main/java/de/dominikwieners/musikvereinhusen/ui/activities/DetailActivity.java
+++ app/src/main/java/de/dominikwieners/musikvereinhusen/ui/activities/DetailActivity.java
@@ -2,6 +2,7 @@ package de.dominikwieners.musikvereinhusen.ui.activities;
 
 import android.graphics.Color;
 import android.os.Build;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -19,17 +20,18 @@ public class DetailActivity extends NucleusBaseAppCompatActivity<DetailPresenter
     @BindView(R.id.detail_toolbar)
     Toolbar toolbar;
 
+    @BindView(R.id.toolbar_layout)
+    CollapsingToolbarLayout collapsingToolbarLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
-        setSupportActionBar(toolbar);
-        toolbar.setTitle("Hallo");
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
 
+        setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
